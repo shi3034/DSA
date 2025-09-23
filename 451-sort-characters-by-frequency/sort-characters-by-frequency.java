bbd15c3f0ef17398a961b8566c290c1cae0f04ec
shi3034 <139464498+shi3034@users.noreply.github.com>
2025-09-24 03:11:23 +0530

    Time: 12 ms (85.85%) | Memory: 46.1 MB (22.09%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,6 +1,6 @@
 class Solution {
     static{
-       for(int i=0;i<500;i++)
+       for(int i=0;i<400;i++)
        frequencySort(new String());
     }
     public static String frequencySort(String s) {
