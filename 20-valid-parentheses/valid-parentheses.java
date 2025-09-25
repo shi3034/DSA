25308c3b6703749f65c5f4da2efedba478297c63
shi3034 <139464498+shi3034@users.noreply.github.com>
2025-09-26 02:05:57 +0530

    Time: 4 ms (19.01%) | Memory: 41.6 MB (89.41%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean isValid(String s) {
+        if(s.length()%2!=0)
+        return false;
         Stack<Character> st=new Stack<>();
         for(int i=0;i<s.length();i++)
         {
