93ffecd8e082a0be6b5e339effb5cf35256bf1d2
shi3034 <139464498+shi3034@users.noreply.github.com>
2025-10-11 06:59:01 +0530

    Time: 5 ms (53.06%) | Memory: 45.1 MB (32.25%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans=new int[2];
+        HashMap<Integer,Integer> hash=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int complement=target-nums[i];
+            if(hash.containsKey(complement))
+            {
+                ans[0]=i;
+                ans[1]=hash.get(complement);
+            }
+            hash.put(nums[i],i);
+        }
+        return ans;
+    }
+}
