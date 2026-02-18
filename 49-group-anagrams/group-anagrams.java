4e0aeab8deceba4f119e0b818d396e6293a39f57
shi3034 <139464498+shi3034@users.noreply.github.com>
2026-02-18 21:11:16 +0530

    Time: 4 ms (99.79%) | Memory: 49.4 MB (96.03%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>> map=new HashMap<>();
         for(String s:strs){
@@ -9,5 +9,24 @@ class Solution {
             map.get(str).add(s);
         }
         return new ArrayList<>(map.values());
+    }
+}*/
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<Long,List<String>> map=new HashMap<>();
+        for(String str:strs){
+            long key=getUniqueKey(str);
+            map.putIfAbsent(key,new ArrayList<>());
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+    public Long getUniqueKey(String str){
+        int[] primes={2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,101,103};
+        long mul=1;
+        for(char ch:str.toCharArray()){
+            mul=mul*primes[ch-'a'+1];
+        }
+        return mul;
     }
 }
