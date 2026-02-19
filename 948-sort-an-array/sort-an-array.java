72c04496dbbb1ea925099b44233d93b4e3eb0c0a
shi3034 <139464498+shi3034@users.noreply.github.com>
2026-02-19 19:35:47 +0530

    Time: 82 ms (16.12%) | Memory: 84.7 MB (43.53%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,25 +1,67 @@
 public class Solution {
-    private void shellSort(int[] nums, int n) {
-        int gap = n / 2;
-        while (gap >= 1) {
-            for (int i = gap; i < n; i++) {
-                int tmp = nums[i];
-                int j = i - gap;
-                while (j >= 0 && nums[j] > tmp) {
-                    nums[j + gap] = nums[j];
-                    j -= gap;
-                }
-                nums[j + gap] = tmp;
+    public int[] sortArray(int[] nums) {
+        ArrayList<Integer> negatives = new ArrayList<>();
+        ArrayList<Integer> positives = new ArrayList<>();
+
+        for (int num : nums) {
+            if (num < 0) {
+                negatives.add(-num);
+            } else {
+                positives.add(num);
             }
-            gap /= 2;
+        }
+
+        if (!negatives.isEmpty()) {
+            radixSort(negatives);
+            Collections.reverse(negatives);
+            for (int i = 0; i < negatives.size(); i++) {
+                negatives.set(i, -negatives.get(i));
+            }
+        }
+
+        if (!positives.isEmpty()) {
+            radixSort(positives);
+        }
+
+        int index = 0;
+        for (int num : negatives) {
+            nums[index++] = num;
+        }
+        for (int num : positives) {
+            nums[index++] = num;
+        }
+        return nums;
+    }
+
+    private void countSort(ArrayList<Integer> arr, int n, int d) {
+        int[] count = new int[10];
+        for (int num : arr) {
+            count[(num / d) % 10]++;
+        }
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        ArrayList<Integer> res = new ArrayList<>(Collections.nCopies(n, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            int idx = (arr.get(i) / d) % 10;
+            res.set(count[idx] - 1, arr.get(i));
+            count[idx]--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr.set(i, res.get(i));
         }
     }
 
-    public int[] sortArray(int[] nums) {
-        int n = nums.length;
-        if (n == 1) return nums;
+    private void radixSort(ArrayList<Integer> arr) {
+        int n = arr.size();
+        int maxElement = Collections.max(arr);
+        int d = 1;
 
-        shellSort(nums, n);
-        return nums;
+        while (maxElement / d > 0) {
+            countSort(arr, n, d);
+            d *= 10;
+        }
     }
 }
