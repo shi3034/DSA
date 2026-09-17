1b46bbf5001454000bfb8b3065c853ed637d0e63
shi3034 <139464498+shi3034@users.noreply.github.com>
2026-09-17 17:46:03 +0530

    Time: 32 ms (76.13%) | Memory: 60.3 MB (11.38%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,38 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(nums);
-        int n = nums.length;
+        int n=nums.length;
+        for(int num=0;num<n-2;num++)
+        {
+            if(num>0 && nums[num]==nums[num-1])
+            continue;
+            int i=num+1,j=n-1;
+            while(i<j)
+            {
+            int sum=nums[i]+nums[j]+nums[num];
+            if(sum==0)
+            {
+                List<Integer> list=new ArrayList<>();
+                list.add(nums[num]);
+                list.add(nums[i]);
+                list.add(nums[j]);
+                ans.add(list);
+                i++;
+                j--;
 
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-
-            int complement = 0 - nums[i];
-            int p = i + 1, q = n - 1;
-
-            while (p < q) {
-                int sum = nums[p] + nums[q];
-
-                if (sum == complement) {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[p]);
-                    list.add(nums[q]);
-                    list.add(nums[i]);
-                    ans.add(list);
-
-                    while (p < q && nums[p] == nums[p + 1])
-                        p++;
-                    while (p < q && nums[q] == nums[q - 1])
-                        q--;
-
-                    p++;
-                    q--;
-                } else if (sum > complement) {
-                    q--;
-                } else {
-                    p++;
-                }
+                while(i<j && nums[i]==nums[i-1])
+                i++;
+                while(i<j && nums[j]==nums[j+1])
+                j--;
+            }
+            else if(sum<0)
+            i++;
+            else
+            j--;
             }
         }
         return ans;
